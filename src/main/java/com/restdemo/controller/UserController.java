dee0f4f20bfaaf01d91fd01894c762b606c0e881
microcow <wooo2385@gmail.com>
2024-08-14 11:50:28 +0900

    그래들/jdk버전 맞추기

--- src/main/java/com/restdemo/controller/UserController.java
+++ src/main/java/com/restdemo/controller/UserController.java
@@ -7,6 +7,7 @@ import com.restdemo.service.JwtService;
 import com.restdemo.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,7 +16,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // 리턴되는 정보가 json 형태로 리턴된다
+@RestController // 리턴되는 정보가 json 형태로 리턴된다 //jdk랑 그래들 버전 통일 (gredle8.9 / jdk22)
 public class UserController {
 
  
@@ -50,7 +51,7 @@ public class UserController {
     public JwtResponseDTO Signup(@RequestBody User user){
     	
     	user.setPassword(passwordEncoder.encode(user.getPassword())); // BCryptPasswordEncoder를 주입받음 (BCryptPasswordEncoder 객체를 생성한 후, encode 메서드를 사용하여 비밀번호를 인코딩)
-    	userservice.createUser(user);
+    	userservice.createUser(user); ////프론트에서 보내는 username의 값이 username에 매칭되지 않네(프론트 문제임)
     	
     	AuthRequestDTO token = new AuthRequestDTO();
     	token.setUsername(user.getUsername());
@@ -58,10 +59,9 @@ public class UserController {
     	
     	return AuthenticateAndGetToken(token);
     }
-   
+    @Secured({"ROLE_USER"})
     @PostMapping("/api/Signin")
     public JwtResponseDTO Signin(@RequestBody User user){ // 프론트에서 값을 전달받지 못하고있음
-    	System.out.println(user); //
     	User getUser = userservice.readUser(user.getUsername());
     	
     	if (getUser.getUsername().equals(user.getUsername()) ||
@@ -82,6 +82,7 @@ public class UserController {
     // @PreAuthorize 어노테이션을 사용하여 특정 메서드가 실행되기 전에 접근 권한을 확인할 수 있습니다.
     // 이 메서드는 ROLE_ADMIN 권한을 가진 사용자만 호출할 수 있음
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Secured({"ROLE_USER"}) // @PreAuthorize와 @Secured의 차이 : @PreAuthorize는 보다 복잡한 표현식 사용 가능
     @GetMapping("/ping")
     public String test() {
         try {
